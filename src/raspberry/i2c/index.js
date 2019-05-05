98e080ac6938748aab15ced5dcdf27037f152bdc
Steponas Dauginis <steponas.dauginis@gmail.com>
2019-05-05 23:39:43 +0300

    Check relays for the expected result
    The BigRelay board sometimes does not turn off the relays as instructed.
    We need to check if the command succeeded, and repeat if not.

--- src/raspberry/i2c/index.js
+++ src/raspberry/i2c/index.js
@@ -2,7 +2,7 @@
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 const { gpioLock } = require('../../common/gpio');
-// const { waitFor } = require('../../common/helpers');
+const { waitFor } = require('../../common/helpers');
 const logger = require('../../common/log');
 const Relays = require('./relays');
 
@@ -12,49 +12,67 @@ const Relays = require('./relays');
 /**
  * Generate the bw_tool command to be run.
  */
-const bwToolCommand = (board, cmd, bit) => `bw_tool -D /dev/i2c-1 -a ${board} -W ${cmd}:${bit}:b`;
+const bwToolWrite = (board, cmd, bit) => `bw_tool -I -D /dev/i2c-1 -a ${board} -W ${cmd}:${bit}:b`;
+const readRelays = board => `bw_tool -I -D /dev/i2c-1 -a ${board} -R 10:b`;
 
 const execute = async (cmd) => {
   logger.log(`Executing "${cmd}"`);
   const unlock = await gpioLock();
+  let result = null;
   try {
-    await exec(cmd);
+    const { stdout, stderr } = await exec(cmd);
+    result = stdout;
   } catch (err) {
     logger.log(`Error executing: ${err}`);
   }
   unlock();
+  return result;
 };
 
 const toggleRelay = async (num, isOn) => {
-  const { boardAddr, relayAddr } = Relays.mapRelay(num);
+  const { boardAddr, relayAddr, relayNum } = Relays.mapRelay(num);
 
-  const cmd = bwToolCommand(boardAddr, relayAddr.toString(16), isOn ? '01' : '00');
-  if (isOn) {
+  // Check if the expected command succeeded toggling the relay.
+  const check = async () => {
+    const checkCmd = readRelays(boardAddr);
+    const data = await execute(checkCmd);
+
+    if (!data) {
+      return false;
+    }
+
+    const num = parseInt(data, 16);
+    const relayBit = 1 << (relayNum - 1);
+    const expResult = isOn ? relayBit : 0;
+
+    return (num & relayBit) === expResult;
+  };
+
+  const cmd = bwToolWrite(boardAddr, relayAddr.toString(16), isOn ? '01' : '00');
+
+  while (true) {
     await execute(cmd);
-  } else {
-    await execute(cmd);
-    // Problems with turning the relays off.
-    // Need to do it slowly, and repeatedly.
-    // for (let n = 1; n <= STOP_COUNT; n += 1) {
-    //   await execute(cmd);
-    //   if (STOP_COUNT !== n) {
-    //     await waitFor(STOP_DELAY);
-    //   }
-    // }
+    await waitFor(1000);
+    const ok = await check();
+    if (ok) {
+      // The relay is in a good state. We're done.
+      break;
+    } else {
+      // Relay didn't toggle.
+      // Wait 2 seconds before trying the same command again.
+      await waitFor(2000);
+    }
   }
 };
 
 const turnAllOff = async () => {
-  await execute(bwToolCommand(Relays.RELAY_1, 10, '00'));
-  await execute(bwToolCommand(Relays.RELAY_2, 10, '00'));
-  // for (let i = 1; i <= 12; i += 1) {
-  //   await toggleRelay(i, false);
-  //   await waitFor(300);
-  // }
+  for (let i = 1; i <= 12; i += 1) {
+    await toggleRelay(i, false);
+    await waitFor(300);
+  }
 };
 
 module.exports = {
-  connect: () => {},
   toggleRelay,
   turnAllOff,
 };
