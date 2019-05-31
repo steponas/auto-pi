b1a62efa1cb6e4fe5c58b9a9fe13e656a82f86ce
Steponas Dauginis <steponas.dauginis@gmail.com>
2019-05-31 22:27:19 +0300

    More move to TS

--- src/raspberry/i2c/__tests__/relays.ts
+++ src/raspberry/i2c/__tests__/relays.ts
@@ -1,12 +1,14 @@
-const Relays = require('../relays');
+/* eslint @typescript-eslint/explicit-function-return-type:0 */
+import Relays, { RelayNum } from '../relays';
 
 it('should generate correct values for all 6 relays', () => {
   for (let i = 1; i <= 6; i += 1) {
-    expect(Relays.getAddr(i)).toEqual(0x20 + i - 1);
+    expect(Relays.getAddr(i as RelayNum)).toEqual(0x20 + i - 1);
   }
 });
 
 it('should throw an error if relay number is too low', () => {
+  // @ts-ignore
   const t = () => Relays.getAddr(0);
   expect(t).toThrow();
 });
@@ -25,6 +27,7 @@ it('should map a low relay number to the first board', () => {
 });
 
 it('should map a high relay number to the first board', () => {
+  // @ts-ignore
   const result = Relays.mapRelay(6 + 5);
 
   expect(result.boardAddr).toEqual(Relays.RELAY_2);
