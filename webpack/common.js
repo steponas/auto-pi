8b4af862508837eac7e8a140788952f0bf2ede90
Steponas Dauginis <steponas.dauginis@gmail.com>
2019-06-16 17:49:12 +0300

    Add antd UI framework

--- webpack/common.js
+++ webpack/common.js
@@ -10,12 +10,16 @@ module.exports = {
   },
   module: {
     rules: [
-      { 
+      {
         test: /\.(t|j)sx?$/, 
         use: { loader: 'awesome-typescript-loader' },
         exclude: /node_modules/,
       },
       { enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' },
+      {
+        test: /\.css$/,
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+      },
     ],
   },
 };
--- webpack/dev.babel.js
+++ webpack/dev.babel.js
@@ -1,5 +1,5 @@
 /* eslint @typescript-eslint/no-var-requires:0 */
-const prodConfig = require('./prod');
+const prodConfig = require('./prod.babel');
 
 module.exports = Object.assign({}, prodConfig, {
   mode: 'development',
