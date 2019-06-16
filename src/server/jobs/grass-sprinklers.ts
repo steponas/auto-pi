9f302995b7340b80490848943b52d34c7c9d8d93
Steponas Dauginis <steponas.dauginis@gmail.com>
2019-06-16 11:45:35 +0300

    More sprinkler adjustments

--- src/server/jobs/grass-sprinklers.ts
+++ src/server/jobs/grass-sprinklers.ts
@@ -1,14 +1,14 @@
-const { toggleRelay } = require('../../raspberry/i2c');
-const { log } = require('../../common/log');  
-const {
+import { toggleRelay } from 'raspberry/i2c';
+import { log } from 'common/log';  
+import {
   FRONT_LAWN, BACK_LAWN_1, BACK_LAWN_2, SLOPE,
-} = require('../../config/relay-names');
-const { waitFor } = require('../../common/helpers');
-const { HOURS, MINUTES, SECONDS } = require('../../common/time');
+} from '../../config/relay-names';
+import { waitFor } from 'common/helpers';
+import { HOURS, MINUTES, SECONDS } from 'common/time';
 
 const jobName = 'Grass Sprinklers';
 
-async function runFrontClose() {
+async function runFrontClose(): Promise<void> {
   await toggleRelay(FRONT_LAWN, true);
   await waitFor(30 * SECONDS);
 
@@ -20,9 +20,54 @@ async function runFrontClose() {
   await waitFor(15 * MINUTES);
 
   const list = [FRONT_LAWN, BACK_LAWN_1, BACK_LAWN_2];
-  for (sprinkler of list) {
+  for (let sprinkler of list) {
     await toggleRelay(sprinkler, false);
     await waitFor(5 * SECONDS);
   }
 }
 
+async function runSeparate(): Promise<void> {
+  const list = [BACK_LAWN_1, BACK_LAWN_2, FRONT_LAWN];
+  for (let sprinkler of list) {
+    await toggleRelay(sprinkler, true);
+    await waitFor(30 * MINUTES);
+    await toggleRelay(sprinkler, false);
+    await waitFor(5 * SECONDS);
+  }
+}
+
+async function runBackClose(): Promise<void> {
+  await toggleRelay(BACK_LAWN_1, true);
+  await waitFor(10 * SECONDS);
+
+  await toggleRelay(BACK_LAWN_2, true);
+  await waitFor(30 * MINUTES);
+
+  const list = [BACK_LAWN_1, BACK_LAWN_2];
+  for (let sprinkler of list) {
+    await toggleRelay(sprinkler, false);
+    await waitFor(5 * SECONDS);
+  }
+}
+
+async function runSlope(): Promise<void> {
+  await toggleRelay(SLOPE, true);
+  await waitFor(3 * HOURS);
+
+  await toggleRelay(SLOPE, false);
+}
+
+const waterLawn = async (): Promise<void> => {
+  log(jobName, 'starting');
+
+  await runFrontClose();
+  await runSeparate();
+  await runBackClose();
+  await runSlope();
+
+  log(jobName, 'done');
+};
+
+module.exports = (setupJob): void => {
+  setupJob(jobName, '0 0 3 * * *', waterLawn, false);
+};
