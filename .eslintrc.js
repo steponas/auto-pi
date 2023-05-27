3efe92172417610d0b88e953df133fea3fd414e0
Steponas Dauginis <steponas.dauginis@gmail.com>
2023-05-27 19:28:44 +0300

    eslint updates

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,11 @@
 module.exports = {
   parser:  '@typescript-eslint/parser',
   env: {
-    browser: true,
     commonjs: true,
     es6: true,
     jest: true,
   },
-  extends: ['plugin:react/recommended', 'plugin:@typescript-eslint/recommended'],
+  extends: ['plugin:@typescript-eslint/recommended'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly'
@@ -14,19 +13,12 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2019,
     sourceType:  'module',
-    ecmaFeatures:  {
-      jsx: true,  // Allows for the parsing of JSX
-    },
   },
   rules: {
     'no-await-in-loop': 0,
     'import/prefer-default-export': 0,
     '@typescript-eslint/indent': ['error', 2],
     '@typescript-eslint/explicit-member-accessibility': 0,
-  },
-  settings:  {
-    react:  {
-      version: 'detect',  // Tells eslint-plugin-react to automatically detect the version of React to use
-    },
+    '@typescript-eslint/explicit-function-return-type': 0,
   },
 };
